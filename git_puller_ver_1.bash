1ea9350533cf2618e9a66b12eef7b55e6bc09f95
Ben Larson <benjaminwlarson@gmail.com>
2019-06-14 00:43:11 -0400

    Added If statement bypassing /kestros-build-helper/ maven clean install

--- git_puller_ver_1.bash
+++ git_puller_ver_1.bash
@@ -5,26 +5,26 @@ read -s -p "Password: " password
 echo $user
 credentials="${user}:${password}"
 
-declare -a repositoryNames=("slingware-root-project"
-				"structured-resource-models"
-				"ui-libraries"
-				"kestros-cms-foundation"
-				"kestros-site-admin-ui"
-				"kestros-forms-foundation"
-				"kestros-login"
-				"kestros-site-administration"
-				"kestros-user-management-core"
-				"kestros-build-helper"
-				)
+declare -a repositoryNames=("kestros-build-helper"
+							"slingware-root-project"
+							"structured-resource-models"
+							"ui-libraries"
+							"kestros-cms-foundation"
+							"kestros-site-admin-ui"
+							"kestros-forms-foundation"
+							"kestros-login"
+							"kestros-site-administration"
+ 							"kestros-user-management-core"
+							)
 
 
 for name in "${repositoryNames[@]}"
 do
-	## Add If Logic to check if folder exists, Create it if not.
-	cd ../$(echo $name)/
-	pwd
-	git pull https://${credentials}@github.com/slingware/${name}.git #>/dev/null 2>&1
-	## if name == credentials[9]
-		## ask if want to mvn clean install
-	mvn clean install -PinstallPackage
+		## Add If Logic to check if folder exists, Create it if not.
+		cd ../$(echo $name)/
+		pwd
+		git pull https://${credentials}@github.com/slingware/${name}.git #>/dev/null 2>&1
+		if [ name != "kestros-build-helper" ]; then
+			mvn clean install -PinstallPackage;
+		fi
 done
